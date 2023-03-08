e6af5a1ed3d2901cf1ce95ff3f3c828723a0571e
Musingila <justdaynoh@gmail.com>
2023-03-08 11:24:02 +0300

    Initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,44 @@
+import React,{useEffect, useState} from 'react'
+
+export function Login() {
+    const [userName, setUserName] = useState('')
+    const [passWord, setPassword] = useState('')
+
+
+  return (
+        <>
+        <h1>Dairy farming management system</h1>
+        <div className="box">
+            <div className="form">
+               
+                <div className="inputBox">
+                    <input type="text" required= "required" />
+                    <span>Username</span>
+                    <i></i>
+                </div>
+                <div className="inputBox">
+                    <input type="password" required= "required" id="" />
+                    <span>Password</span>
+                    <i></i>
+                </div>
+
+                <div className="links">
+                    <a href="#">Forgot Password</a>
+                    <a href="#">Signup</a>
+                </div>
+                <input type="submit" value="Login" />
+                
+                
+
+            </div>
+
+            
+        </div> 
+        </>
+        
+        
+      
+  )
+}
+
+export default Login
